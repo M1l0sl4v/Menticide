736fe42c76a702957d3b9c794820869c4df7f560
M1l0sl4v <145215447+M1l0sl4v@users.noreply.github.com>
2024-09-02 22:20:42 -0400

    Enemyobject destory at certain distance

--- Assets/SCripts/enemy.cs
+++ Assets/SCripts/enemy.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     public float enemyspeed;
-    public float despawnTime = .2f;
+    public float despawnDistance = 10;
     
     void Update()
     {
         transform.Translate(new Vector2(0, enemyspeed) * Time.deltaTime);
+        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerPos = player.transform.position;
+        Vector3 currentPos = transform.position;
+        float dist = Vector3.Distance(currentPos, playerPos);
+        if (dist > despawnDistance)
+        {
+            ObjectPoolManager.ReturnObjectToPool(gameObject);
+        }
     }
     // hmmmm
     private void OnCollisionEnter2D(Collision2D collision)
