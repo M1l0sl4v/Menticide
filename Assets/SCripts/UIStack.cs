228fd696d4da4e8a8a54b9307c3b8b318f645c30
TippyToo <wm.titus2@gmail.com>
2024-09-06 13:12:13 -0400

    Implemented framework for UI stack manager

--- Assets/SCripts/UIStack.cs
+++ Assets/SCripts/UIStack.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class UIStack : MonoBehaviour
 {
+
+    private Stack<GameObject> stack = new Stack<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,27 @@ public class UIStack : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Push(GameObject uiElement)
+    {
+        stack.Push(uiElement);
+        uiElement.SetActive(true);
+    }
+
+    public GameObject Pop()
+    {
+        GameObject uiElement = stack.Pop();
+        uiElement.SetActive(false);
+        return uiElement;
+    }
+
+    public void PopAll()
+    {
+        foreach (GameObject uiElement in stack)
+        {
+            uiElement.SetActive(false);
+        }
+        stack.Clear();
     }
 }
