73911789db351fa04b6b1d75597dcf99f9d798bb
M1l0sl4v <145215447+M1l0sl4v@users.noreply.github.com>
2024-09-09 16:14:15 -0400

    new background

--- Assets/SCripts/seasons.cs
+++ Assets/SCripts/seasons.cs
@@ -8,6 +8,8 @@ public class seasons : MonoBehaviour
     public static seasons instance;
     public Camera cameraMain;//this sets the imported camera to cameraMain
     public int season;
+
+    public GameObject backGround;
     // this is whats used to store the season 1=summer,2=fall,3=winter,4=spring
     void Start()
     {
@@ -45,22 +47,26 @@ public class seasons : MonoBehaviour
     //each season has a method here, its set up so we should be able to kinda go ham with the seasons.
     public void summer()
     {
-        cameraMain.backgroundColor = new Color(14f / 255,48f / 255, 7f / 255, 1);//we devide by 255 to get the rgb value
+        backGround.GetComponent<Renderer>().material.color = new Color(14f / 255,48f / 255, 7f / 255, 1);//we devide by 255 to get the rgb value
+
         season = 2; //passes this to fall
     }
     public void fall()
     {
-        cameraMain.backgroundColor = new Color(48f / 255, 32f / 255, 7f / 255, 1);
+        backGround.GetComponent<Renderer>().material.color = new Color(48f / 255, 32f / 255, 7f / 255, 1);//we devide by 255 to get the rgb value
+
         season = 3;//passes this to winter
     }
     public void winter()
     {
-        cameraMain.backgroundColor = new Color(200f / 255, 194f / 255, 186f / 255, 1);
+        backGround.GetComponent<Renderer>().material.color = new Color(200f / 255, 194f / 255, 186f / 255, 1);//we devide by 255 to get the rgb value
+
         season = 4; //passes this to spring
     }
     public void spring()
     {
-        cameraMain.backgroundColor = new Color(118f / 255, 114f / 255, 24f / 255, 1);
+        backGround.GetComponent<Renderer>().material.color = new Color(118f / 255, 114f / 255, 24f / 255, 1);//we devide by 255 to get the rgb value
+
         season = 1;// loops back to summer
     }
     
