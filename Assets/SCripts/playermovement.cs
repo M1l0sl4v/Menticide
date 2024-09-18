13ab051878acb7ed55d638a117b7f668685507af
M1l0sl4v <145215447+M1l0sl4v@users.noreply.github.com>
2024-09-18 02:44:05 -0400

    bug fix and windows build plugin

--- Assets/SCripts/playermovement.cs
+++ Assets/SCripts/playermovement.cs
@@ -71,8 +71,6 @@ public class playermovement : MonoBehaviour
     {
        GameManager.instance.backToZero(resetTriggerDistance);
         seasons.instance.seasonChange();
-        
-
     }
     
     
