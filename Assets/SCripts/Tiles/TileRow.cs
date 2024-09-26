8e6a783278a3ca35515af1e188fcb7cd51d9e399
TippyToo <wm.titus2@gmail.com>
2024-09-25 20:25:44 -0400

    TileRow first usuable state

--- Assets/SCripts/Tiles/TileRow.cs
+++ Assets/SCripts/Tiles/TileRow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,7 +22,7 @@ public class TileRow : MonoBehaviour
         
     }
 
-    // Add empty tiles to the front of the row
+    // Add n empty tiles to the front of the row
     public void AddTilesFront(int n)
     {
         GameObject[] temp = new GameObject[count + n];
@@ -33,7 +34,117 @@ public class TileRow : MonoBehaviour
         {
             temp[n+i] = tiles[i];
         }
-
+        count += n;
         tiles = temp;
     }
+
+    // Add n existing tiles to the front of the row
+    public void AddTilesFront(int n, GameObject[] tilesToAdd)
+    {
+        GameObject[] temp = new GameObject[count + n];
+        for (int i = 0; i < n; i++)
+        {
+            temp[i] = tilesToAdd[i];
+        }
+        for (int i = 0; i < count; i++)
+        {
+            temp[n + i] = tiles[i];
+        }
+        count += n;
+        tiles = temp;
+    }
+
+    // Add n empty tiles to the back of the row
+    public void AddTilesBack(int n)
+    {
+        GameObject[] temp = new GameObject[count + n];
+        for (int i = 0; i < count; i++)
+        {
+            temp[i] = tiles[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            temp[n+i] = null;
+        }
+        count += n;
+        tiles = temp;
+    }
+
+    // Add n existing tiles to the back of the row
+    public void AddTilesBack(int n, GameObject[] tilesToAdd)
+    {
+        GameObject[] temp = new GameObject[count + n];
+        for (int i = 0; i < count; i++)
+        {
+            temp[i] = tiles[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            temp[n + i] = tilesToAdd[i];
+        }
+        count += n;
+        tiles = temp;
+    }
+
+    // Remove n tiles from the front of the row
+    public void RemoveTilesFront(int n)
+    {
+        GameObject[] temp = new GameObject[count - n];
+
+        for (int i = n; i < count; i++)
+        {
+            temp[i-n] = tiles[i];
+        }
+    }
+
+    // Remove n tiles from the back of the row
+    public void RemoveTilesBack(int n)
+    {
+        GameObject[] temp = new GameObject[count - n];
+
+        for (int i = n; i < count; i++)
+        {
+            temp[i - n] = tiles[i];
+        }
+    }
+
+
+    // Return tile GameObject at the specified index
+    public GameObject GetTile(int index)
+    {
+        return tiles[index];
+    }
+
+    // Return full array of tiles in the row
+    public GameObject[] GetTiles()
+    {
+        return tiles;
+    }
+
+    // Return a selected slice of the tile array
+    public GameObject[] GetTiles(int frontBoundInclusive, int backBoundExclusive)
+    {
+        if (backBoundExclusive <= frontBoundInclusive || frontBoundInclusive < 0)
+        {
+            throw new ArgumentException("frontBoundInclusive must be greater than 0 and less than backBoundExclusive");
+        }
+
+        GameObject[] temp = new GameObject[backBoundExclusive - frontBoundInclusive];
+
+        for (int i = frontBoundInclusive; i < backBoundExclusive; i++)
+        {
+            temp[i - frontBoundInclusive] = tiles[i];
+        }
+
+        return temp;
+    }
+
+    // Return the number of tiles in the row
+    public int Count()
+    {
+        return count;
+    }
+
+
+    
 }
