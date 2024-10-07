f9b128d8f30ef650b37d8085f5339da9b35e7fde
Schmerz <cschmerzler@gmail.com>
2024-10-07 10:21:48 -0400

    Started Created Main Menu
    Having problems with on click. might be problem with scene manager vs game manager? not sure will find out

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+}
