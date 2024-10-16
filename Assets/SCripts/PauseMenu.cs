547fead1a1d6bf3e2fc6909b38bfd0f91e6a9537
TippyToo <wm.titus2@gmail.com>
2024-10-16 11:54:34 -0400

    Revert "Merge branch 'main' into season-transitions"
    This reverts commit 3f63328a7cc5c423f597eae55626718a50c8f799, reversing
    changes made to 95b20f3040062ecd7418b6cbd9b4e224424aae58.

--- Assets/SCripts/PauseMenu.cs
+++ Assets/SCripts/PauseMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public static PauseMenu instance;
+
     public bool paused;
     
     public GameObject pauseMenu;
--- Assets/SCripts/Tiles/TileManagerFSM.cs
+++ Assets/SCripts/Tiles/TileManagerFSM.cs
@@ -75,9 +75,9 @@ public class TileManagerFSM : MonoBehaviour
     void Start()
     {
         instance = this;
-        
-        ApplyEnums();
-        
+
+        SetEnums();
+
         // Compile big list
         masterList = ConcatArrays(
 
@@ -280,33 +280,6 @@ public class TileManagerFSM : MonoBehaviour
                 if (tile.name == "Overlay 0") tile.direction = Direction.Left;
                 else if (tile.name == "Overlay 1" || tile.name == "Overlay 2") tile.direction = Direction.Middle;
                 else if (tile.name == "Overlay 3") tile.direction = Direction.Right;
-            }
-        }
-    }
-    // This function shouldn't have to exist, but Unity saves enums in a weird way so this should be called once every time the scene is loaded
-    public void ApplyEnums()
-    {
-        Transform tilemap = GameObject.Find("Tilemap").transform;
-
-        foreach (Transform row in tilemap)
-        {
-            Transform baseTiles = row.Find("Base");
-            Transform overlayTiles = row.Find("Overlay");
-
-            foreach (TileObject tile in baseTiles.GetComponentsInChildren<TileObject>())
-            {
-                tile.tileLayer = TileObject.TileLayer.Base;
-                if (tile.name == "Base 0") tile.spriteType = TileObject.SpriteType.Left;
-                else if (tile.name == "Base 1" || tile.name == "Base 2") tile.spriteType = TileObject.SpriteType.Middle;
-                else if (tile.name == "Base 3") tile.spriteType = TileObject.SpriteType.Right;
-            }
-
-            foreach (TileObject tile in overlayTiles.GetComponentsInChildren<TileObject>())
-            {
-                tile.tileLayer = TileObject.TileLayer.Overlay;
-                if (tile.name == "Overlay 0") tile.spriteType = TileObject.SpriteType.Left;
-                else if (tile.name == "Overlay 1" || tile.name == "Overlay 2") tile.spriteType = TileObject.SpriteType.Middle;
-                else if (tile.name == "Overlay 3") tile.spriteType = TileObject.SpriteType.Right;
             }
         }
     }
