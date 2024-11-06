ea126fa410bf1c62ef66752d38c6ef60fb070884
TippyToo <wm.titus2@gmail.com>
2024-11-06 17:24:03 -0500

    Clear old debug

--- Assets/SCripts/trees/tree.cs
+++ Assets/SCripts/trees/tree.cs
@@ -67,7 +67,6 @@ public class tree : MonoBehaviour
         if (leafsinstance != null)
         {
             //Destroy(leafsinstance);  
-            Debug.LogWarning("Cleared leaves");
             leafsinstance.Clear();
         }
     }
