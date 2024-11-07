d7a7f58914d79fa93b6586ce8aefedffa962a661
M1l0sl4v <145215447+M1l0sl4v@users.noreply.github.com>
2024-11-07 17:08:58 -0500

    tree shakes and small tweaks

---
+++ Assets/SCripts/objectShake.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class objectShake : MonoBehaviour
+{
+    public float shakeTime;
+    public float shakeAmount;
+
+    private Vector2 ogPosition;
+
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ogPosition = transform.localPosition;
+        if (other.CompareTag("Player"))
+        { 
+            shakeObject();
+            Debug.Log("hitplayer");
+        }
+        
+    }
+
+    public void shakeObject()
+    {
+        StartCoroutine(shake());
+    }
+
+
+    private IEnumerator shake()
+    {
+        float elapsed = 0.0f;
+
+
+        while (elapsed < shakeTime)
+        {
+            float offsetX = Random.Range(-1f, 1f) * shakeAmount;
+            float offsetY = Random.Range(-1f, 1f) * shakeAmount;
+            
+            transform.localPosition = ogPosition + new Vector2(offsetX, offsetY);
+            
+            elapsed += Time.deltaTime;
+            
+            Debug.Log("shaken");
+            
+            yield return null;
+        }
+        
+        transform.localPosition = ogPosition;
+        
+    }
+    
+    
+    
+    void Update()
+    {
+        
+    }
+}
