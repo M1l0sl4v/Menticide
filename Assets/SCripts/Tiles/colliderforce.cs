b8fad7c99420fecda03c59727341ccb8cf8a5f0b
M1l0sl4v <145215447+M1l0sl4v@users.noreply.github.com>
2025-01-02 17:23:21 -0500

    the collision with the borders are not working

--- Assets/SCripts/Tiles/colliderforce.cs
+++ Assets/SCripts/Tiles/colliderforce.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class colliderforce : MonoBehaviour
 {
     public float pushforce = 10f;
-
-    private void OnTriggerStay(Collider other)
+    
+    public void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Rigidbody rb = other.GetComponent<Rigidbody>();
-            if (rb != null)
-            { Vector3 pushDirection = other.transform.position - transform.position;
-                rb.AddForce(pushDirection.normalized * pushforce, ForceMode.Impulse); // Adjust the force value as needed
-            }
+            Vector3 pushDirection = other.transform.position - transform.position; 
+            rb.AddForce(pushDirection.normalized * pushforce, ForceMode.Impulse); // Adjust the force value as needed
+            Debug.Log("player touched");
         }
     }
 }
